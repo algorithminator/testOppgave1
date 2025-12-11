358d40916d36d22a9e8d494317b92ae0dff2adc8
Frederic Dorn <83763956+algorithminator@users.noreply.github.com>
2025-12-11 21:19:59 +0100

    Update TestAssignment.java

--- src/test/java/assignment/TestAssignment.java
+++ src/test/java/assignment/TestAssignment.java
@@ -25,5 +25,42 @@ public class TestAssignment extends BaseAssignment
     @Test()
     public void task01() {
         // Code that tests the assignment goes here
+         testClass("assignment.Hello", () -> {
+            var printOut = "Hello World!";
+            var hello = classCreateInstance();
+
+            classInstanceInvokeMethod(hello, "printhei");
+
+            assertStandardOutputEquals(printOut);
+        });
+
+
+
+        testClassMethod("assignment","Hello", "printhei", () -> {
+
+            assertFalse(methodIsStatic());
+
+            assertTrue(methodHasModifiers(AccessFlag.PUBLIC));
+
+            assertTrue(methodReturns(String.class));
+
+            // assertion
+            //assertEquals("Hoho", bos.toString());
+
+            /*PrintStream originalOut = System.out;
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(bos));
+
+            // action
+            Hello.main(null);
+
+            // assertion
+            assertEquals("Hello world!\n", bos.toString());
+
+            // undo the binding in System
+            System.setOut(originalOut);
+            */
+
+        });
     }
 }
